f540f454f1a7d101e5419cbd9fe204b3f66f050e
unknown <tttr@cnu1071s8y.bkkhq.chevrontexaco.net>
2014-05-07 22:25:49 +0700

    plot1

---
+++ plot1.R
@@ -0,0 +1,25 @@
+    # read in the data table with header TRUE, column separator as ";" and column classes as specified
+
+    column_classes = c("character","character","numeric","numeric","numeric","numeric","numeric","numeric","numeric")
+    data_frame <- read.table("../household_power_consumption.txt", 
+                             header = TRUE, 
+                             sep = ";", 
+                             colClasses = column_classes, 
+                             na.strings = "?"
+                             )
+    
+    nrows <- nrow(data_frame)
+    
+    # convert 1st column from character to date
+    date <- as.Date(data_frame[,1], "%d/%m/%Y")
+    
+    # make a histogram of column 3 (Global Active Power)
+    hist(data_frame[date == "2007-02-01" | date == "2007-02-02", 3], 
+         col = "red", 
+         main = "Global Active Power", 
+         xlab = "Global Active Power (kilowatts)"
+         )
+    
+    # copy the plot to a png file and close the device
+    dev.copy(png, file = "plot1.png")
+    dev.off()
